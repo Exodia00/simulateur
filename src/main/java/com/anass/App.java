6d25672cc484af082f19809305b1b8ed4913d24f
alami <medtahaalami@gmail.com>
2023-08-08 15:15:51 +0100

    updated the launch sequence

--- src/main/java/com/anass/App.java
+++ src/main/java/com/anass/App.java
@@ -2,6 +2,8 @@ package com.anass;
 
 import java.io.IOException;
 
+import com.anass.controllers.SetupController;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.geometry.Pos;
@@ -18,15 +20,8 @@ public class App extends Application {
 
     public void start(Stage stage) throws IOException {
 
-        String fxml = "/com/anass/fxml/setup.fxml";
-
-        FXMLLoader loader = new FXMLLoader();
-        Parent root = (Parent) loader.load(getClass().getResourceAsStream(fxml));
-        Scene scene = new Scene(root);
-
-        stage.setTitle("Nouvelle simulation");
-        stage.setScene(scene);
-        stage.show();
+        SetupController controller = new SetupController();
+        controller.start();
     }
 
     public static void main(String[] args) {
